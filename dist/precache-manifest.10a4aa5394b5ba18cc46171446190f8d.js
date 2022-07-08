8b4f3b964101470dc7c5837e4a2c2b0367f1a1bb
Magomedgadzhii <108467549+Magomedgadzhii@users.noreply.github.com>
2022-07-08 17:45:15 +0300

    Add files via upload

---
+++ dist/precache-manifest.10a4aa5394b5ba18cc46171446190f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a31bcab0e76ca53d397",
+    "url": "/css/app.a6491a62.css"
+  },
+  {
+    "revision": "d37bd90f0c7e73c26244a255f342bb43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a31bcab0e76ca53d397",
+    "url": "/js/app.b9708529.js"
+  },
+  {
+    "revision": "3164cd4ae9cfa0298c37",
+    "url": "/js/chunk-vendors.f666c2f3.js"
+  },
+  {
+    "revision": "ffa1ed858ab71257554bc4d4f3e07d7b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
